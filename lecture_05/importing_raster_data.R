a28e5c9e814e53c96272dd693fdd0e596cc0a991
MagdaHa <m.halbgewachs@online.de>
2018-11-13 15:35:15 +0100

    lecture 05: added script about importing raster data

---
+++ lecture_05/importing_raster_data.R
@@ -0,0 +1,26 @@
+#creating an empty raster dataset r1
+library(raster)
+r1 <- raster(nrows=10, ncols=10)
+#filling empty raster with 100 random values
+r1[] <- rnorm(100) # [] uses r1 and replaces the empty raster with random data
+r1
+plot(r1)
+#or
+r1[] <- df$measure1[1:100]
+plot(r1)
+
+#create a second raster dataset r2
+r2 <- raster(nrows=10, ncols=10)
+#filling empty raster with 100 random values
+r2[] <- rnorm(100) # [] uses r1 and replaces the empty raster with random data
+r1
+plot(r2)
+#or
+r2[] <- df$measure2[1:100]
+plot(r2)
+
+library(raster)
+dgm <- raster("C:/Bachelorarbeit/Aster GDEM/Arbeitsgebiet_clip.tif")
+plot(dgm)
+
+vector <- 
