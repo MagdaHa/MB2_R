429d96d3afc5fea25c16d04b27df1d78f5533140
MagdaHa <m.halbgewachs@online.de>
2018-11-16 12:02:53 +0100

    lecture 05: corrections in raster data script

--- lecture_05/importing_raster_data.R
+++ lecture_05/importing_raster_data.R
@@ -1,3 +1,8 @@
+#############################
+####importing raster data####
+#############################
+
+##creating own raster
 #creating an empty raster dataset r1
 library(raster)
 r1 <- raster(nrows=10, ncols=10)
@@ -19,8 +24,8 @@ plot(r2)
 r2[] <- df$measure2[1:100]
 plot(r2)
 
+#-----------------------------------------------------------------------------------
+##loading pregenerated raster, like satellite imagery
 library(raster)
 dgm <- raster("C:/Bachelorarbeit/Aster GDEM/Arbeitsgebiet_clip.tif")
 plot(dgm)
-
-vector <- 
