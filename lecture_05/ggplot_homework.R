c4ffa177b3ba2789015de506676ed9d5c6af6682
MagdaHa <m.halbgewachs@online.de>
2018-11-19 16:29:17 +0100

    lecture 05: adding new visualization to ggplot2 scatter plot

--- lecture_05/ggplot_homework.R
+++ lecture_05/ggplot_homework.R
@@ -1,19 +1,30 @@
 ###########################
 ####Homework lecture 05####
 ###########################
-library(ggplot2)
+
 
 #set Working Directory
 setwd("C:\\02_Studium\\02_Master\\01_Semester 1\\MB2_Introduction to Programming and Geostatistics_Wegmann\\MB2_R\\lecture_05")
 
+#------------------------------------------------------------------------------------------------------------------------------------
 #loading data
 Census.Data <- read.csv("practical_data.csv")
 names(Census.Data)
 
+#------------------------------------------------------------------------------------------------------------------------------------
+#loading library ggplot2
+library(ggplot2)
+
+#------------------------------------------------------------------------------------------------------------------------------------
 #scatter plot with ggplot2
 p <- ggplot(Census.Data, aes(Unemployed, Qualification))
 p + geom_point()
+
+#-----------------------------------------------------------
 #scatter plot with good visualization
-p <- ggplot(Census.Data, aes(Unemployed, Qualification))
-p + geom_point(aes(colour = White_British, size = Low_Occupancy))+
-  labs(title="Relationship between age and unemployment in the UK")
+p <- ggplot(Census.Data, aes(Unemployed, Qualification))+
+p + geom_point(aes(colour = White_British, size = Low_Occupancy))+                    #displayed variables, shown in size and colour
+  labs(title="Correlation between age and occupancy \n in relation to qualification and unemployment rate in the UK")+   #title
+  theme_dark()+                                  #background colour
+  theme(legend.position = "right")               #legend position
+
