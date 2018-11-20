a451fb02cb7d619ca0d7e4b6d8950ed2be3c6d4c
MagdaHa <m.halbgewachs@online.de>
2018-11-20 16:35:16 +0100

    lecture 01: added script about time difference calculations

---
+++ lecture_01/fun_difftime.R
@@ -0,0 +1,7 @@
+##############################
+####time to a defined date####
+##############################
+
+difftime("2018-12-24", Sys.Date(), units="days")
+difftime("2018-12-24", Sys.Date(), units="weeks")
+difftime("2018-12-24", Sys.Date(), units="mins")
