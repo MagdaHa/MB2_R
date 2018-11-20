14d4dccdcdc2ece0b6c764851aedd4d38c6fe845
MagdaHa <m.halbgewachs@online.de>
2018-11-20 16:36:45 +0100

    lecture 06: added fun script about sun calculations

---
+++ lecture_06/fun_sun_calculation.R
@@ -0,0 +1,13 @@
+#######################
+####sun calculation####
+#######################
+####https://github.com/datastorm-open/suncalc####
+
+install.packages("suncalc")
+install.packages("V8")
+
+library(suncalc)
+library(V8)
+
+#-----------------------------------------------------------------------------------------
+getSunlightTimes (date = Sys.Date(), lat = 49.782332, lon = 9.970187, tz = "CET")
