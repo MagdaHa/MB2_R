9cee65a98950c73e23cf4cc9c8f32dca76120ff1
MagdaHa <m.halbgewachs@online.de>
2018-12-04 12:08:07 +0100

    magda: new package called magda

---
+++ magda/R/addValue.R
@@ -0,0 +1,20 @@
+#'@description A function that add values.
+#'
+#'@param x one value
+#'@param y another value
+#'
+#'@details this function adds entered values
+#'
+#'@return numeric value 
+#'
+#'@examples val <- addValue(1, 3)
+#'
+#'@export
+
+addValue <- function(x, y){
+  return(x, y)
+}
+
+
+
+
