202f7273e8e6844151a49b0172ae11b809891d12
MagdaHa <m.halbgewachs@online.de>
2019-02-16 16:48:59 +0100

    lecture 11: change detection

--- lecture_11/change_detection.R
+++ lecture_11/change_detection.R
@@ -41,3 +41,66 @@ plot(cv_2018)
 ##minus calculatin of two images
 change_18 <- NDWI_0718_crop - NDWI_0418_crop # or vise versa
 plot(change_18)
+
+
+
+
+library(gganimate)
+# Only show constructed wind turbines
+REPD_wind <-
+  REPD %>%
+  filter(`Technology Type` == "Wind Onshore") %>%
+  filter(Status.Summary == "Approved")
+
+# Create gif
+invisible(saveGIF({
+  for (i in 1990:2017){
+    
+    p <- 
+      ggplot(data = NULL, aes(x = `X-coordinate`, y = `Y-coordinate`)) +
+      geom_polygon(data = UK, aes(long, lat, group = group), fill = "#528076") +
+      geom_point(data = filter(REPD_wind, year < i), 
+                 fill = "#b2d1e0", size = 3, alpha = 0.8, shape = 21) +
+      geom_point(data = filter(REPD_wind, year == i), 
+                 fill = "gold", size = 5, shape = 21) +
+      coord_equal(xlim = c(-150000, 900000), ylim = c(0, 1200000)) +
+      labs(title = "Location of Installed Projects") +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            axis.ticks = element_blank(),
+            panel.background = element_rect(fill = "grey99", colour = "grey80"),
+            plot.title = element_text(hjust = 0.5))
+    
+    p2 <- 
+      ggplot(filter(REPD_wind, year <= i), aes(x = year, y = `Installed Capacity (MWelec)`)) +
+      geom_col(aes(fill= year >= i)) +
+      scale_fill_manual(values = c("#b2d1e0","gold")) +
+      scale_x_continuous(limits = c(1990, 2018), expand = c(0,0)) +
+      scale_y_continuous(limits = c(0, 2000), expand = c(0,0)) +
+      labs(title = "Annual Installed Capacity", x = "Year", y = "Installed Capacity (MW)") +
+      theme(legend.position = "none",
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_rect(fill = "grey99", colour = "grey80"),
+            plot.title = element_text(hjust = 0.5))
+    
+    
+    p3 <- ggplot(data = NULL, aes(x = 1990:2017, y = 1)) +
+      geom_line() +
+      geom_point(aes(fill = (x = 1990:2017 > i)), shape = 21, size = 5) +
+      theme_void() +
+      theme(legend.position = "none") +
+      scale_fill_manual(values = c("#b2d1e0","gold")) +
+      geom_text(aes(x = i, y = 1, label = i), vjust = -1, size = 9) +
+      theme(panel.background = element_rect(fill = "grey99", colour = "grey80"))
+    
+    
+    
+    # Print plots using patchwork
+    print(p + p2 - p3 + plot_layout(ncol = 1, heights = c(5, 1)))
+  }
+  
+  
+}, movie.name = "windDevelopment.gif", interval = 1, ani.width = 1000, ani.height = 700))
