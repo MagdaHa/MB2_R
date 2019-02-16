435442b8b0815799013b2d889ac116a9fb2cdeda
MagdaHa <m.halbgewachs@online.de>
2019-02-16 16:48:44 +0100

    lecture 10: supervised classification

--- lecture_10/supervised_classification.R
+++ lecture_10/supervised_classification.R
@@ -9,13 +9,16 @@ library(rgdal)
 
 #loading satellite image to clssify
 image <- brick("C:\\02_Studium\\02_Master\\01_Semester 1\\MB1_Digital Image Analysis and GIS_Hüttich\\04_data\\Steigerwald\\03_raster\\01_landsat\\02_timescan\\TimeScan_EAGLE_AOI_UTM_WGS84.tif")
+image_crop <- crop()
 #plot(image)
 
 #loading training data
 td <- rgdal::readOGR("C:\\02_Studium\\02_Master\\01_Semester 1\\MB1_Digital Image Analysis and GIS_Hüttich\\01_Uebungen\\classification_steigerwald\\TimeScan_trainingsamples.shp")
 #plot(td)
 
+
 #create supervised classification, save as tif
+setwd("C:\\Users\\Magdalena\\Documents")
 sc <- superClass(image, trainData = td, responseCol = "Classvalue", filename = "myClassification.tif")  #responseCol: name of the column where samples are stored
 plot(sc$map)
 
@@ -24,5 +27,12 @@ plot(sc$map)
 validateMap(sc$map, mode = "classification")
 rasterEntropy(image)
 
+#validation
 aa1 <- superClass(image, trainData=td, responseCol = "Classvalue", trainPartition = 0.7)    # training classes don't have to be merged!
-a1$calidation$performance
+a1$validation$performance
+
+#validating maps with training data
+#validateMap()
+
+#validating different classifications
+#rasterEntropy()
